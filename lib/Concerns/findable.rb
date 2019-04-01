8c0b7153a48e98df70b7e88bce2dba11b13a4863
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-01 00:22:26 +0000

    Automatically backed up by Learn

--- lib/Concerns/findable.rb
+++ lib/Concerns/findable.rb
@@ -1,20 +1,9 @@
-require 'bundler'
-Bundler.require
+module Concerns::Findable
+  def find_by_name(name)
+    self.all.detect{|s| s.name == name}
+  end
 
-module Concerns
-  module Findable
-    def find_by_name(name)
-      self.all.detect {|object| object.name == name}
-    end
-
-    def find_or_create_by_name(name)
-      if self.find_by_name(name).nil?
-        self.create(name)
-      else
-        self.find_by_name(name)
-      end
-    end
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
 end
-
-require_all 'lib'
